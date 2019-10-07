bffc7973ff232dd19c57d2db4b190890496d8aae
Mark Basmayor <mark.basmayor@gmail.com>
2019-10-07 22:11:14 +0800

    [Go Track] Update solution for _Scrabble_
    * Address code review comment: Replace init code with hard-coded letter to value map.

--- go/scrabble-score/scrabble.go
+++ go/scrabble-score/scrabble.go
@@ -17,32 +17,41 @@ import (
 	"strings"
 )
 
-var scoreMap = map[rune]int{}
-
-func init() {
-	var scoresForLetters = map[int]string{
-		1:  "AEIOULNRST",
-		2:  "DG",
-		3:  "BCMP",
-		4:  "FHVWY",
-		5:  "K",
-		8:  "JX",
-		10: "QZ",
-	}
-
-	for score, letters := range scoresForLetters {
-		for _, letter := range letters {
-			scoreMap[letter] = score
-		}
-	}
-}
-
 // Score calculates the total score for a word in scrabble.
 func Score(word string) int {
+	letterValues := map[rune]int{
+		'A': 1,
+		'B': 3,
+		'C': 3,
+		'D': 2,
+		'E': 1,
+		'F': 4,
+		'G': 2,
+		'H': 4,
+		'I': 1,
+		'J': 8,
+		'K': 5,
+		'L': 1,
+		'M': 3,
+		'N': 1,
+		'O': 1,
+		'P': 3,
+		'Q': 10,
+		'R': 1,
+		'S': 1,
+		'T': 1,
+		'U': 1,
+		'V': 4,
+		'W': 4,
+		'X': 8,
+		'Y': 4,
+		'Z': 10,
+	}
+
 	word = strings.ToUpper(word)
 	total := 0
 	for _, char := range word {
-		total += scoreMap[char]
+		total += letterValues[char]
 	}
 	return total
 }
